b2ee084593c633461cbe4e3d58920634298921c6
ibrahim ahmed <133769074+himahamoda@users.noreply.github.com>
2024-06-07 12:16:36 +0400

    Update nav.jsx

--- src/nav/nav.jsx
+++ src/nav/nav.jsx
@@ -51,7 +51,7 @@ export default function NAv() {
 
             <li><a href="/">Home</a></li>
 
-            <li><a href="/Products">Products</a></li>
+            <li><a href="/products">Products</a></li>
 
           </ul>
           <div className="menus text-light ">
