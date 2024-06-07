aa002d3fcb380775be34f7276562e1512b7b827f
ibrahim ahmed <133769074+himahamoda@users.noreply.github.com>
2024-06-07 13:01:21 +0400

    Update nav.jsx

--- src/nav/nav.jsx
+++ src/nav/nav.jsx
@@ -52,7 +52,7 @@ export default function NAv() {
 
 {/*             <li><a href="/">Home</a></li>
  */}
-           <Link to={`/`} className='text-black' > <li>Home</li> </Link>
+           <Link to={`/`} className='text-black me-3' > <li>Home</li> </Link>
 
            <Link to={`/products`} className='text-black' > <li>Gallery</li> </Link>
 
